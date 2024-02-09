ed76d75c47b145cfa30018dfb80b8739da657d3e
Sԋҽɾɾყ 'ʂ <sherry_macbook@Sqs-MacBook-Pro.local>
2024-02-09 11:58:37 +0500

    Fixed cloudinary and added some snaps

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,6 +1,6 @@
 Cloudinary.config do |config|
-  config.cloud_name = ENV["cloudinary_cloud_name"]
-  config.api_key = ENV["cloudinary_api_key"]
-  config.api_secret = ENV["cloudinary_api_secret"]
-  config.cdn_subdomain = true
+  config.cloud_name = 'dl4cox4og'
+  config.api_key = '983614348756411'
+  config.api_secret = 'cQ0ibM-lt6A4uQb5SON0V2SJQN4'
+  config.secure = true
 end
